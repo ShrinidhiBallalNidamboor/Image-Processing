dd1d86239abe1dfd6c309cab59205fa9136673cc
Shrinidhi <ballalshrinidhi@gmail.com>
2024-01-07 16:15:23 +0530

    .

--- Image Processing.py
+++ Image Processing.py
@@ -13,8 +13,17 @@ for i in range(m):
     for j in range(n):
         for k in range(o):
             image_duplicate[i][j][k]=(image[i][j][k]-image_duplicate[i][j][k])%256
-            image_enhanced[i][j][k]=255-image_enhanced[i][j][k]*2
             image[i][j][k]=(255-image[i][j][k])%256
+for i in range(m):
+    for j in range(n):
+        temp=max([image_enhanced[i][j][0], image_enhanced[i][j][1], image_enhanced[i][j][2]])
+        if temp>=255/2:
+            image_enhanced[i][j][0]+=40
+            image_enhanced[i][j][1]+=40
+            image_enhanced[i][j][2]+=40
+        image_enhanced[i][j][0]*=255/295
+        image_enhanced[i][j][1]*=255/295
+        image_enhanced[i][j][2]*=255/295
 cv2.imwrite("reverse.png", image)
 cv2.imwrite("subtraction.png", image_duplicate)
 cv2.imwrite("enhanced.png", image_enhanced)
