7439c0ccaf5776c2ecb47b9804db2337010dbda2
Shrinidhi <ballalshrinidhi@gmail.com>
2024-01-11 21:45:36 +0530

    .

--- Greyscale/greyscale.py
+++ Greyscale/greyscale.py
@@ -7,7 +7,7 @@ n=len(image[0])
 o=len(image[0][0])
 for i in range(m):
     for j in range(n):
-        temp=0.299*image[i][j][0]+0.587*image[i][j][1]+0.114*image[i][j][2]
+        temp=0.114*image[i][j][0]+0.587*image[i][j][1]+0.299*image[i][j][2]
         image[i][j][0]=temp
         image[i][j][1]=temp
         image[i][j][2]=temp
--- Pseudo Coloring/color.py
+++ Pseudo Coloring/color.py
@@ -8,7 +8,7 @@ o=len(image[0][0])
 for i in range(m):
     for j in range(n):
         temp=image[i][j][0]*3
-        image[i][j][0]=temp*0.2126
+        image[i][j][0]=temp*0.0722
         image[i][j][1]=temp*0.587
-        image[i][j][2]=temp*0.0722
+        image[i][j][2]=temp*0.2126
 cv2.imwrite("color.png", image)
