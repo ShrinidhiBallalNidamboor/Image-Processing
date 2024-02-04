a7ad798dfba62711f665de2814d1babb4d7ac90e
Shrinidhi <ballalshrinidhi@gmail.com>
2024-02-04 17:45:16 +0530

    .

--- Property_Convolution/Image Processing.py
+++ Property_Convolution/Image Processing.py
@@ -2,10 +2,8 @@ import cv2
 import numpy as np
 
 image = cv2.imread('image.png')
-
-m=len(image)
-n=len(image[0])
-o=len(image[0][0])
+image = np.array(image)
+scalar = 0.5
 
 kernel=np.array([
     [2/7, 1/7, 0],
@@ -13,5 +11,8 @@ kernel=np.array([
     [0, 1/7, 1/7]
 ])
 
-duplicate=cv2.filter2D(image, -1, kernel) 
-cv2.imwrite("duplicate.png", duplicate)
+duplicate1=cv2.filter2D(scalar*image, -1, kernel) 
+duplicate2=cv2.filter2D(image, -1, scalar*kernel) 
+
+cv2.imwrite("duplicate1.png", duplicate1)
+cv2.imwrite("duplicate2.png", duplicate2)
