8b5a098ba3f235938fd159b51cb1a9988d994759
Shrinidhi <ballalshrinidhi@gmail.com>
2024-02-12 22:16:00 +0530

    .

--- Enhance_grey/Enhance.py
+++ Enhance_grey/Enhance.py
@@ -8,25 +8,24 @@ m=len(image)
 n=len(image[0])
 o=len(image[0][0])
 
-array = [0]*256
-count=0
+array=[0]*256
 
+count=m*n
 for i in range(m):
     for j in range(n):
         array[image[i][j][0]]+=1
-        count+=1
 
 for i in range(256):
     array[i]/=count
 for i in range(1, 256):
     array[i]+=array[i-1]
 for i in range(256):
-    array[i]=i*array[i]
+    array[i]*=i
 
 for i in range(m):
     for j in range(n):
-        image[i][j][0]=array[image[i][j][0]]
-        image[i][j][1]=array[image[i][j][1]]
-        image[i][j][2]=array[image[i][j][2]]
+        image[i][j][0]=np.round(array[image[i][j][0]])
+        image[i][j][1]=np.round(array[image[i][j][1]])
+        image[i][j][2]=np.round(array[image[i][j][2]])
 
 cv2.imwrite("enhanced.png", image)
