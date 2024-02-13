da88bcaa6375b81e9c0b84d98ea0d7dbdcdc57de
Shrinidhi <ballalshrinidhi@gmail.com>
2024-02-13 23:35:55 +0530

    .

--- Edge-Detection/central.py
+++ Edge-Detection/central.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('image.png')
+image = cv2.imread('image.jpg')
 image = np.array(image)
 
 m=len(image)
@@ -15,7 +15,8 @@ duplicate2=cv2.filter2D(image, -1, kernel)
 
 for i in range(m):
     for j in range(n):
-        for k in range(o):
-            image[i][j][k]=(duplicate1[i][j][k]**2+duplicate2[i][j][k]**2)**0.5
+        image[i][j][0]=(duplicate1[i][j][0]**2+duplicate2[i][j][0]**2)**0.5
+        image[i][j][1]=(duplicate1[i][j][1]**2+duplicate2[i][j][1]**2)**0.5
+        image[i][j][2]=(duplicate1[i][j][2]**2+duplicate2[i][j][2]**2)**0.5
 
-cv2.imwrite("edge.png", image)
+cv2.imwrite("central.png", image)
---
+++ Edge-Detection/sobel.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('image.jpg')
+image = np.array(image)
+
+m=len(image)
+n=len(image[0])
+o=len(image[0][0])
+
+kernel=np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+duplicate1=cv2.filter2D(image, -1, kernel)
+kernel=np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+duplicate2=cv2.filter2D(image, -1, kernel)
+
+for i in range(m):
+    for j in range(n):
+        image[i][j][0]=(duplicate1[i][j][0]**2+duplicate2[i][j][0]**2)**0.5
+        image[i][j][1]=(duplicate1[i][j][0]**2+duplicate2[i][j][0]**2)**0.5
+        image[i][j][2]=(duplicate1[i][j][0]**2+duplicate2[i][j][0]**2)**0.5
+
+cv2.imwrite("sobel.png", image)
