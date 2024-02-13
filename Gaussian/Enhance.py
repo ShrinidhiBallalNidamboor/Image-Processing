98d697130277b4f7caef91e99184daf825d79435
Shrinidhi <ballalshrinidhi@gmail.com>
2024-02-14 00:21:49 +0530

    .

--- Gaussian/Enhance.py
+++ Gaussian/Enhance.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 
 image = cv2.imread('Image.png')
-image_duplicate = cv2.imread('Image.png')
 image = np.array(image)
-image_duplicate = np.array(image_duplicate)
 
 m=len(image)
 n=len(image[0])
@@ -18,22 +16,25 @@ def getValue(i, j, k):
 def getGaussian(std, x, y):
     return np.exp(-(x**2+y**2)/(2*std**2))/(2*3.14*std**2)
 
-kernel=10
+def Gaussian(size, std):
+    array=[]
+    for i in range(size):
+        value=[]
+        for j in range(size):
+            value.append(0)
+        array.append(value)
+    value=size//2
+    for i in range(size):
+        for j in range(size):
+            array[i][j]=getGaussian(std, i-value, j-value)
+    return array
 
-for i in range(m):
-    for j in range(n):
-        data = []
-        for a in range(i-kernel, i+kernel+1):
-            for b in range(j-kernel, j+kernel+1):
-                data.append(getValue(a, b, 0))
-        std=np.round(np.std(data))
-        value=0
-        for a in range(i-kernel, i+kernel+1):
-            for b in range(j-kernel, j+kernel+1):
-                index=getGaussian(std, i-a, j-b)
-                value+=getValue(a, b, 0)*index
-        image[i][j][0]=value
-        image[i][j][1]=value
-        image[i][j][2]=value
+std1=1
+std2=std1-0.01
 
-cv2.imwrite("result.png", image_duplicate)
+a=np.array(Gaussian(5, std1))
+b=np.array(Gaussian(5, std2))
+kernel=a-b
+
+duplicate=cv2.filter2D(image, -1, kernel)
+cv2.imwrite("result.png", duplicate)
