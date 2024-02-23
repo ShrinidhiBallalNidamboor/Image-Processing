3818b5a2bdf89f24221eff3cab5909a4fd4f8d39
Shrinidhi <ballalshrinidhi@gmail.com>
2024-02-23 22:18:42 +0530

    .

--- Combined/Border/code.py
+++ Combined/Border/code.py
@@ -6,6 +6,6 @@ kernel=np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 result, threshold=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 dilate=cv2.dilate(threshold, kernel, iterations=1)
-image=dilate-threshold
-dilate=cv2.dilate(image, kernel, iterations=1)
-cv2.imwrite('border.png', dilate)
+erode=cv2.erode(threshold, kernel, iterations=1)
+image=dilate-erode
+cv2.imwrite('border.png', image)
--- Combined/Laplacian/code.py
+++ Combined/Laplacian/code.py
@@ -7,7 +7,7 @@ image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 kernel=np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 structural_element=np.array([[1, 1], [1, 1]])
 
-gaussian=cv2.GaussianBlur(image, (5, 5), 2)
+gaussian=cv2.GaussianBlur(image, (3, 3), 2)
 border1=cv2.filter2D(gaussian, -1, kernel)
 result, border1=cv2.threshold(border1, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 border1=cv2.erode(border1, structural_element, iterations=1)
@@ -16,7 +16,7 @@ border1=cv2.erode(border1, structural_element, iterations=1)
 border1=cv2.dilate(border1, structural_element, iterations=1)
 cv2.imwrite('border1.png', border1)
 
-gaussian1=cv2.GaussianBlur(image, (7, 7), 2)
+gaussian1=cv2.GaussianBlur(image, (3, 3), 2)
 gaussian2=cv2.GaussianBlur(image, (5, 5), 2)
 border2=gaussian2-gaussian1
 result, border2=cv2.threshold(border2, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
