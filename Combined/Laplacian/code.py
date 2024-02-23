84241bea8e7b4f485e29dae09e48d75d44a883b1
Shrinidhi <ballalshrinidhi@gmail.com>
2024-02-23 12:59:55 +0530

    .

--- Combined/Laplacian/code.py
+++ Combined/Laplacian/code.py
@@ -2,17 +2,28 @@ import cv2
 import numpy as np
 
 image=cv2.imread('image.png')
-distance=0.0001
+image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-image1=cv2.GaussianBlur(image, (5, 5), distance)
-image2=cv2.GaussianBlur(image, (5, 5), 0)
+kernel=np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+structural_element=np.array([[1, 1], [1, 1]])
 
-image=-image1+image2
-cv2.imwrite('border1.png', image)
+gaussian=cv2.GaussianBlur(image, (5, 5), 2)
+border1=cv2.filter2D(gaussian, -1, kernel)
+result, border1=cv2.threshold(border1, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+border1=cv2.erode(border1, structural_element, iterations=1)
+border1=cv2.dilate(border1, structural_element, iterations=1)
+border1=cv2.erode(border1, structural_element, iterations=1)
+border1=cv2.dilate(border1, structural_element, iterations=1)
+cv2.imwrite('border1.png', border1)
 
-image=cv2.imread('image.png')
-
-image=cv2.GaussianBlur(image, (5, 5), distance)
-kernel=np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
-image=cv2.filter2D(image, -1, kernel)
-cv2.imwrite('border2.png', image)
+gaussian1=cv2.GaussianBlur(image, (7, 7), 2)
+gaussian2=cv2.GaussianBlur(image, (5, 5), 2)
+border2=gaussian2-gaussian1
+result, border2=cv2.threshold(border2, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+m=len(image)
+n=len(image[0])
+border2=cv2.erode(border2, structural_element, iterations=1)
+border2=cv2.dilate(border2, structural_element, iterations=1)
+border2=cv2.erode(border2, structural_element, iterations=1)
+border2=cv2.dilate(border2, structural_element, iterations=1)
+cv2.imwrite('border2.png', border2)
