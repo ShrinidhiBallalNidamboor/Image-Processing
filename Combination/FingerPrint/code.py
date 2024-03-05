83acf215f609013cabff5380790aff3c8f494030
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-05 12:49:33 +0530

    .

---
+++ Combination/FingerPrint/code.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+kernel=np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]).astype("uint8")
+image=cv2.imread('image.png')
+image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+result, image=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+m=len(image)
+n=len(image[0])
+for i in range(m):
+    for j in range(n):
+        image[i][j]=255-image[i][j]
+image=cv2.morphologyEx(image, cv2.MORPH_HITMISS, kernel)
+cv2.imwrite('fingerprint.png', image)
