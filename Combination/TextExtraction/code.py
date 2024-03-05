d0a42e943075dd28b002fa0eabdd9d70fa665157
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-05 13:01:49 +0530

    .

---
+++ Combination/TextExtraction/code.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+image=cv2.imread('image.png')
+image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+clahe=cv2.createCLAHE(clipLimit=2)
+image=clahe.apply(image)
+result, image=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+cv2.imwrite('binary.png', image)
