d1edcf212be5759c4007d3c80d286456948661fa
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-07 14:39:32 +0530

    .

--- Combination/Complete/Water Level/code.py
+++ Combination/Complete/Water Level/code.py
@@ -1,50 +1,39 @@
 import cv2
 import numpy as np
 
-kernel=np.array([[1, 1], [1, 1]])
 full=cv2.imread('full.png')
 half=cv2.imread('half.png')
 quarter=cv2.imread('quarter.png')
 empty=cv2.imread('empty.png')
 
-full=cv2.cvtColor(full, cv2.COLOR_BGR2GRAY)
-half=cv2.cvtColor(half, cv2.COLOR_BGR2GRAY)
-quarter=cv2.cvtColor(quarter, cv2.COLOR_BGR2GRAY)
-empty=cv2.cvtColor(empty, cv2.COLOR_BGR2GRAY)
-
-full=cv2.resize(full, (200, 500))
-half=cv2.resize(half, (200, 500))
-quarter=cv2.resize(quarter, (200, 500))
-empty=cv2.resize(empty, (200, 500))
+full=cv2.resize(full, (220, 500), 3)
+half=cv2.resize(half, (220, 500), 3)
+quarter=cv2.resize(quarter, (220, 500), 3)
+empty=cv2.resize(empty, (220, 500), 3)
 
 full=full-empty
 half=half-empty
 quarter=quarter-empty
 
-m=len(full)
-n=len(full[0])
-for i in range(m):
-    for j in range(n):
-        if full[i][j]>200:
-            full[i][j]=255
-        else:
-            full[i][j]=0
-for i in range(m):
-    for j in range(n):
-        if half[i][j]>200:
-            half[i][j]=255
-        else:
-            half[i][j]=0
-for i in range(m):
-    for j in range(n):
-        if quarter[i][j]>200:
-            quarter[i][j]=255
-        else:
-            quarter[i][j]=0
+full=cv2.cvtColor(full, cv2.COLOR_BGR2GRAY)
+half=cv2.cvtColor(half, cv2.COLOR_BGR2GRAY)
+quarter=cv2.cvtColor(quarter, cv2.COLOR_BGR2GRAY)
 
-full=cv2.erode(full, kernel, iterations=4)
-half=cv2.erode(half, kernel, iterations=4)
-quarter=cv2.erode(quarter, kernel, iterations=4)
+result, full=cv2.threshold(full, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+result, half=cv2.threshold(half, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+result, quarter=cv2.threshold(quarter, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+kernel=np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]).astype("uint8")
+
+#Opening
+full=cv2.erode(full, kernel, iterations=3)
+full=cv2.dilate(full, kernel, iterations=3)
+
+half=cv2.erode(half, kernel, iterations=3)
+half=cv2.dilate(half, kernel, iterations=3)
+
+quarter=cv2.erode(quarter, kernel, iterations=3)
+quarter=cv2.dilate(quarter, kernel, iterations=3)
 
 (number, label, values, centroid)=cv2.connectedComponentsWithStats(full, 4, cv2.CV_32S)
 maximum1=[0, 0]
@@ -54,7 +43,6 @@ for i in range(1, number):
         maximum1[0]=area
         maximum1[1]=i
 full=(label==maximum1[1]).astype("uint8")*255
-
 (number, label, values, centroid)=cv2.connectedComponentsWithStats(half, 4, cv2.CV_32S)
 maximum2=[0, 0]
 for i in range(1, number):
@@ -63,7 +51,6 @@ for i in range(1, number):
         maximum2[0]=area
         maximum2[1]=i
 half=(label==maximum2[1]).astype("uint8")*255
-
 (number, label, values, centroid)=cv2.connectedComponentsWithStats(quarter, 4, cv2.CV_32S)
 maximum3=[0, 0]
 for i in range(1, number):
@@ -73,13 +60,9 @@ for i in range(1, number):
         maximum3[1]=i
 quarter=(label==maximum3[1]).astype("uint8")*255
 
-full=cv2.dilate(full, kernel, iterations=4)
-half=cv2.dilate(half, kernel, iterations=4)
-quarter=cv2.dilate(quarter, kernel, iterations=4)
-
 cv2.imwrite('binary1.png', full)
 cv2.imwrite('binary2.png', half)
 cv2.imwrite('binary3.png', quarter)
 
-print('Half volume: ', np.round(maximum2[0]*100/maximum1[0]))
-print('Quarter volume: ', np.round(maximum3[0]*100/maximum1[0]))
+print('Percentage half: ', np.round(maximum2[0]*100/maximum1[0]))
+print('Percentage quarter: ', np.round(maximum3[0]*100/maximum1[0]))
--- Combination/TextExtraction/code.py
+++ Combination/TextExtraction/code.py
@@ -3,7 +3,8 @@ import numpy as np
 
 image=cv2.imread('image.png')
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-clahe=cv2.createCLAHE(clipLimit=2)
+clahe=cv2.createCLAHE(clipLimit=10)
 image=clahe.apply(image)
+cv2.imwrite('enhanced.png', image)
 result, image=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 cv2.imwrite('binary.png', image)
