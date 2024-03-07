894a46e4fefe52d09a730dd4702e5516ff9d8ed7
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-07 15:32:42 +0530

    .

--- Combination/Complete/Brain Component/code.py
+++ Combination/Complete/Brain Component/code.py
@@ -1,25 +1,27 @@
 import cv2
 import numpy as np
 
-kernel=np.array([[1, 1, 1], [1, 1, 1]])
 image=cv2.imread('Brain.png')
-temp=cv2.imread('Brain.png')
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+temp=cv2.imread('Brain.png')
 temp=cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
-result, image=cv2.threshold(image, 50, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-image=cv2.erode(image, kernel, iterations=2)
-cv2.imwrite('binary.png', image)
+m=len(image)
+n=len(image[0])
+for i in range(m):
+    for j in range(n):
+        if image[i][j]>90:
+            image[i][j]=255
+        else:
+            image[i][j]=0
 (number, label, values, centroid)=cv2.connectedComponentsWithStats(image, 4, cv2.CV_32S)
 maximum=[0, 0]
 for i in range(1, number):
     area=values[i, cv2.CC_STAT_AREA]
-    if area>maximum[0]:
+    if maximum[0]<area:
         maximum[0]=area
         maximum[1]=i
 component=(label==maximum[1]).astype("uint8")*255
-component=cv2.dilate(component, kernel, iterations=2)
-m=len(temp)
-n=len(temp[0])
+cv2.imwrite('binary.png', component)
 for i in range(m):
     for j in range(n):
         if component[i][j]==0:
