1652bf0f97f0d82d2772c5b632a4e42bf9fd7a5c
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-07 21:20:56 +0530

    .

--- Combined/Histogram Equaliser Gray/code.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-image=cv2.imread('image.png')
-image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-image=cv2.equalizeHist(image)
-image=cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-cv2.imwrite('enhance.png', image)
