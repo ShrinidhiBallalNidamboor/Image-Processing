ee94680dfacc1a6af3d3a7e64854f4826776790c
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-07 18:16:37 +0530

    .

--- Combination/Complete/Finger Print/code.py
+++ Combination/Complete/Finger Print/code.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-kernel=np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]).astype("uint8")
+kernel=np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
 image=cv2.imread('image.png')
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 result, image=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
