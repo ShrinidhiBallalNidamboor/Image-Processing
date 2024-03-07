81c9be3a545c2c266d749867ce0294b67268845e
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-07 16:43:35 +0530

    .

--- Combination/Complete/Thresholding/code.py
+++ Combination/Complete/Thresholding/code.py
@@ -3,17 +3,17 @@ import numpy as np
 
 image=cv2.imread('image.png')
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-threshold=127
 histogram=[0]*256
 m=len(image)
 n=len(image[0])
 for i in range(m):
     for j in range(n):
         histogram[image[i][j]]+=1
+threshold=123
 for i in range(100):
     a=0
-    b=0
     count1=0
+    b=0
     count2=0
     for j in range(threshold+1):
         a+=j*histogram[j]
@@ -23,7 +23,7 @@ for i in range(100):
         count2+=histogram[j]
     a/=count1
     b/=count2
-    threshold=int((a+b)//2)
+    threshold=int(np.round((a+b)/2))
 for i in range(m):
     for j in range(n):
         if image[i][j]<=threshold:
@@ -31,17 +31,24 @@ for i in range(m):
         else:
             image[i][j]=255
 cv2.imwrite('binary1.png', image)
+
 image=cv2.imread('image.png')
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-x=0
-y=255
+histogram=[0]*256
+m=len(image)
+n=len(image[0])
+for i in range(m):
+    for j in range(n):
+        histogram[image[i][j]]+=1
+x=50
+y=200
 for i in range(100):
     a=0
-    b=0
     count1=0
+    b=0
     count2=0
     for j in range(256):
-        if np.abs(histogram[j]-x)<=np.abs(histogram[j]-y):
+        if np.abs(j-x)<=np.abs(j-y):
             a+=j*histogram[j]
             count1+=histogram[j]
         else:
@@ -49,9 +56,10 @@ for i in range(100):
             count2+=histogram[j]
     x=a/count1
     y=b/count2
+
 for i in range(m):
     for j in range(n):
-        if np.abs(x-image[i][j])<=np.abs(y-image[i][j]):
+        if np.abs(image[i][j]-x)<=np.abs(image[i][j]-y):
             image[i][j]=x
         else:
             image[i][j]=y
