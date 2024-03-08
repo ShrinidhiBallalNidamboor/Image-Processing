ff250ef8c9d47ab574899c2965947c883384745e
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-08 23:01:33 +0530

    .

---
+++ Combination/Local-Thresholding-Block/code.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+image=cv2.imread('image.png')
+image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+m, n=image.shape
+kernel=30
+start=[30, 30]
+for i in range(start[0], m+kernel, 2*kernel):
+    for j in range(start[1], n+kernel, 2*kernel):
+        temp=image[max([i-kernel, 0]):min([i+kernel, m-1]),max([j-kernel, 0]):min([j+kernel, n-1])]
+        result, temp=cv2.threshold(temp, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        image[max([i-kernel, 0]):min([i+kernel, m-1]),max([j-kernel, 0]):min([j+kernel, n-1])]=temp
+
+cv2.imwrite('binary.png', image)
