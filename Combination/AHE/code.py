46b8123b0e70610f1b760fd4100874807d83c837
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-08 16:02:23 +0530

    .

--- Combination/AHE/code.py
+++ Combination/AHE/code.py
@@ -3,6 +3,8 @@ import numpy as np
 
 image=cv2.imread('image.png')
 image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-clahe=cv2.createCLAHE(clipLimit=5)
+m=len(image)
+n=len(image[0])
+clahe=cv2.createCLAHE(clipLimit=m*n+1, tileGridSize=(7, 7))
 image=clahe.apply(image)
 cv2.imwrite('enhance.png', image)
