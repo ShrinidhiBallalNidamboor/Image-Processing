68eb3ddb8723d93177de324a610ffade876dd425
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-08 15:51:11 +0530

    .

--- Brightness/brightness.py
+++ Brightness/brightness.py
@@ -1,19 +1,19 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('nature.jpg')
-m=len(image)
-n=len(image[0])
-o=len(image[0][0])
+image=cv2.imread('image.png')
+image=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+gray=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+m=len(gray)
+n=len(gray[0])
 for i in range(m):
     for j in range(n):
-        image[i][j][0]+=5
-        image[i][j][1]+=5
-        image[i][j][2]+=5
-        if image[i][j][0]>=255:
-            image[i][j][0]=255
-        if image[i][j][1]>=255:
-            image[i][j][1]=255
-        if image[i][j][2]>=255:
-            image[i][j][2]=255
-cv2.imwrite("brightness.jpg", image)
+        gray[i][j]=np.log2(gray[i][j]+1)
+for i in range(m):
+    for j in range(n):
+        gray[i][j]+=100
+for i in range(m):
+    for j in range(n):
+        image[i][j][2]=gray[i][j]
+image=cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+cv2.imwrite('brightness.jpg', image)
