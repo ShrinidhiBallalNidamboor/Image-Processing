635adc145b6a73815529991b0207af38fc0acdd5
Shrinidhi <ballalshrinidhi@gmail.com>
2024-03-09 11:47:49 +0530

    .

--- Combination/Correlation/code.py
+++ Combination/Correlation/code.py
@@ -3,8 +3,14 @@ import numpy as np
 
 image=cv2.imread('image.png')
 kernel=cv2.imread('kernel.png')
-image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-kernel=cv2.cvtColor(kernel, cv2.COLOR_BGR2GRAY)
-image=cv2.filter2D(image, -1, kernel)
-result, image=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+image=cv2.matchTemplate(image, kernel, cv2.TM_CCOEFF_NORMED)
+low, high, lowcord, highcord=cv2.minMaxLoc(image)
+m, n=image.shape
+maximum=0
+for i in range(m):
+    for j in range(n):
+        maximum=max([maximum, image[i][j]])
+for i in range(m):
+    for j in range(n):
+        image[i][j]=np.log2(image[i][j]+1)*255/np.log2(maximum+1)
 cv2.imwrite('shape.png', image)
--- Combination/Skeleton/code.py
+++ Combination/Skeleton/code.py
@@ -1,12 +1,17 @@
 import cv2
 import numpy as np
 
-image=cv2.imread('image.png')
-image=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image=cv2.imread('image.png', 0)
 result, image=cv2.threshold(image, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+skel=np.zeros(image.shape, np.uint8)
 kernel=cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-for i in range(5):
+while True:
     open=cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-    sub=image-open
-    close=cv2.erode(image, kernel, iterations=1)
-    cv2.bitwise_or(close, temp)
+    temp=cv2.subtract(image, open)
+    erode=cv2.erode(image, kernel)
+    skel=cv2.bitwise_or(skel, temp)
+    image=erode.copy()
+    if cv2.countNonZero(image)==0:
+        break
+
+cv2.imwrite('thin.png', skel)
